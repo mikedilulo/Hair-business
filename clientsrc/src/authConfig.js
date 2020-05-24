2c59d20308a8c75c19108d8bc775a6afe9b8633e
mikedilulo <mikedilulo2019@gmail.com>
2020-05-24 14:00:23 -0600

    Add Domain for Auth0 Configuration

--- clientsrc/src/authConfig.js
+++ clientsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
+export const domain = "madcodeworks.auth0.com";
 export const audience = "https://hairstyles-business.com";
 export const clientId = "LeQ87I2eMXMYy8e7oSjo0Uzh67nL87yT";
